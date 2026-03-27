371bb8abe4018f108efd42cc0287fda13f0a0d55
suryansh1808 <suryansh.saxena_cs24@gla.ac.in>
2026-03-28 04:11:34 +0530

    added models to admin page

--- Lab/admin.py
+++ Lab/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Lab, LabSession, LabActivity, LabScore
 
-# Register your models here.
+
+@admin.register(Lab)
+class LabAdmin(admin.ModelAdmin):
+    list_display = ['title', 'difficulty', 'duration_minutes', 'max_score', 'is_active']
+    list_filter = ['difficulty', 'is_active']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(LabSession)
+class LabSessionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'lab', 'status', 'terraform_state', 'started_at', 'timer_expires_at']
+    list_filter = ['status', 'terraform_state']
+    readonly_fields = ['console_password']
+
+
+@admin.register(LabActivity)
+class LabActivityAdmin(admin.ModelAdmin):
+    list_display = ['session', 'task_name', 'is_passed', 'checked_at']
+    list_filter = ['is_passed']
+
+
+@admin.register(LabScore)
+class LabScoreAdmin(admin.ModelAdmin):
+    list_display = ['user', 'lab', 'passed_tasks', 'total_tasks', 'score_percentage', 'scored_at']
+    list_filter = ['lab']
