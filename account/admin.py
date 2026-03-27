5b085d19f8d5e07f1023ca59ba45ac10e6198f1f
suryansh1808 <suryansh.saxena_cs24@gla.ac.in>
2026-03-28 04:04:53 +0530

    added the models to admin page

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'total_labs_completed', 'average_score', 'current_streak']
+    readonly_fields = ['created_at']
