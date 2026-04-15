c23ce70c1f8ac0c9f5b27b85fffad77e2c351022
suryansh1808 <suryansh.saxena_cs24@gla.ac.in>
2026-04-16 02:00:39 +0530

    updated the urls

--- Lab/urls.py
+++ Lab/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ec2_lab, name='EC2_Lab'),
-    path('start/', views.start_lab, name='start_lab'),
-    path('submit/', views.submit_lab, name='submit_lab'),
-    path('status/', views.lab_status, name='lab_status'),
-    path('validate/', views.submit_lab, name='validate_lab'),  # alias for backward compat
-    path('end/', views.end_lab, name='end_lab'),
+    path('<slug:slug>/', views.lab_detail, name='lab_detail'),
+    path('<slug:slug>/start/', views.start_lab, name='start_lab'),
+    path('<slug:slug>/submit/', views.submit_lab, name='submit_lab'),
+    path('<slug:slug>/status/', views.lab_status, name='lab_status'),
+    path('<slug:slug>/end/', views.end_lab, name='end_lab'),
 ]
