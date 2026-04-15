7600144148f6a54866532e5df7dc568692b2af29
suryansh1808 <suryansh.saxena_cs24@gla.ac.in>
2026-04-16 01:53:14 +0530

    added urls for the teacher dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('teacher/', views.teacher_dashboard, name='teacher_dashboard'),
+    path('teacher/create-teacher/', views.create_teacher, name='create_teacher'),
+    path('teacher/student/<int:student_id>/', views.student_profile, name='student_profile'),
 ]
