f8dce308adb146d2913780684204e00a51053f1c
suryansh1808 <suryansh.saxena_cs24@gla.ac.in>
2026-04-15 23:48:44 +0530

    added Student activity for admin

--- account/admin.py
+++ account/admin.py
@@ -1,8 +1,20 @@
+
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, StudentActivityLog
 
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'total_labs_completed', 'average_score', 'current_streak']
+    list_display = ['user', 'is_teacher', 'total_labs_completed', 'average_score', 'current_streak']
+    list_editable = ['is_teacher']
+    list_filter = ['is_teacher']
     readonly_fields = ['created_at']
+
+
+@admin.register(StudentActivityLog)
+class StudentActivityLogAdmin(admin.ModelAdmin):
+    list_display = ['user', 'activity_type', 'description', 'timestamp']
+    list_filter = ['activity_type', 'timestamp']
+    search_fields = ['user__username', 'description']
+    readonly_fields = ['timestamp']
+    ordering = ['-timestamp']
