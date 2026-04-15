cb6efc2870a8a8861c70fc5a76b8e65a0e951654
suryansh1808 <suryansh.saxena_cs24@gla.ac.in>
2026-04-16 02:17:13 +0530

    bug fixed

--- Terraform/IAM/variables.tf
+++ Terraform/IAM/variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   description = "AWS region"
   default     = "us-east-1"
 }
+
+variable "student_id" {
+  description = "Student ID or username for the lab session"
+  type        = string
+}
